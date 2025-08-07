e1ed0a9831bc1a1be596ec479d35801e2aa96ca9
polio <poliopkilo@yandex.ru>
2025-08-07 15:45:44 +0300

    .

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,10 +17,23 @@
                         {{ __('Календарь') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('equipment')" :active="request()->routeIs('equipment')">
-                        {{ __('Склад') }}
-                    </x-nav-link>
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <div x-data="{ open: false }" class="relative">
+                        <button @click="open = !open" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                            <div>Каталоги</div>
+                            <div class="ms-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                        <div x-show="open" @click.outside="open = false" x-transition class="absolute origin-center-0 mt-2 w-40 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-600 rounded-md shadow-lg z-50">
+                            <x-dropdown-link :href="route('equipment')">
+                                {{ __('Склад') }}
+                            </x-dropdown-link>
+                            </form>
+                        </div>
+                    </div>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('personnel')" :active="request()->routeIs('personnel')">
